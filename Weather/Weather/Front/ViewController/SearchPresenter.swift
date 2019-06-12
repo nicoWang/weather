e62a62d0b96ae96cf6be135990bcec99c3a7b4cc
Nicolas Wang <nicohrlz@gmail.com>
2019-06-12 16:05:55 -0300

    searchviewcontroller created

---
+++ Weather/Weather/Front/ViewController/SearchPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  SearchPresenter.swift
+//  Weather
+//
+//  Created by Nicolas on 12/06/2019.
+//  Copyright © 2019 Nicolas. All rights reserved.
+//
+
+import Foundation
+
+class SearchPresenter: Presenter {
+    
+}
---
+++ Weather/Weather/Front/ViewController/SearchViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SearchViewController.swift
+//  Weather
+//
+//  Created by Nicolas on 12/06/2019.
+//  Copyright © 2019 Nicolas. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: BaseViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var searchPresenter: SearchPresenter!
+    
+    override var presenter: Presenter {
+        return searchPresenter
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    private func customizeUI() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+}
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
